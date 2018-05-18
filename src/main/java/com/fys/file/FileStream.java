e1b48102be723cae88cc15edb5a538cf1790a944
Administrator <849485789@qq.com>
2018-05-18 20:03:32 +0800

    Initial commit

---
+++ src/main/java/com/fys/file/FileStream.java
@@ -0,0 +1,96 @@
+package com.fys.file;
+
+import java.io.*;
+
+/**
+ *  IO 读取文件内容
+ */
+public class FileStream {
+    //要操作的文件名
+    private static String fileName = "E:/sql.sql";
+
+    public static void main(String[] args) {
+        File file = new File(fileName);
+        //fileByReadLine(file);
+        readFileByBytes(file);
+    }
+
+    /**
+     *     每次读取一行
+     */
+    private static void fileByReadLine(File file) {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String str;
+            while(null != (str= bufferedReader.readLine())) {
+                System.out.println(str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            close(fileInputStream);
+        }
+    }
+
+    /**
+     *  通过字节流读取文件内容
+     * @param file
+     */
+    private static void readFileByBytes(File file) {
+        FileInputStream fileInputStream = null;
+        long size = 0;
+        try {
+            fileInputStream = new FileInputStream(file);
+            byte[] bytes = new byte[1024];
+            int len;
+            while(-1 != (len = fileInputStream.read(bytes))) {
+                size += len;
+                System.out.write(bytes, 0, len);
+
+                // 写入到文件内
+                writeFile(bytes, len);
+            }
+            System.out.println("文件长度为:" +size+ "字节");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+           close(fileInputStream);
+        }
+    }
+
+    /**
+     *  将内容写入到一个文件内
+     */
+    private static void writeFile(byte[] bytes, int len) {
+        File file = new File("E:/testWrite.txt");
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(bytes, 0, len);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     *  关闭 FileInputStream
+     */
+    private static void close(FileInputStream fileInputStream) {
+        if(null != fileInputStream) {
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/fys/file/IteratorFileSize.java
@@ -0,0 +1,60 @@
+package com.fys.file;
+
+import java.io.File;
+
+/**
+ * IO 遍历文件夹下的文件，不遍历子文件夹
+ */
+public class IteratorFileSize {
+    static long min = Integer.MAX_VALUE;
+    static long max = 0;
+    static File minFile ;
+    static File maxFile ;
+
+    public static void main(String[] args) {
+        String fileName = "C:/WINDOWS";
+        File file = new File(fileName);
+        if(file.exists()) {
+            File[] files = file.listFiles();
+            for (File fil : files) {
+                if(fil.isFile() && fil.exists()) {
+                    if(max < fil.length()) {
+                        max = fil.length();
+                        maxFile = fil;
+                    }
+                    if(0 != fil.length() && min > fil.length()) {
+                        min = fil.length();
+                        minFile = fil;
+                    }
+                } else if (fil.isDirectory()) {
+                    System.out.println("遍历子文件夹Start 文件夹名称："+fil.getName());
+                    File[] chiFiles = fil.listFiles();
+                    if(null != chiFiles) {
+                        iteratorFile(chiFiles);
+                    } else {
+                        System.out.println("您无权限访问此文件夹! 文件夹名称: "+fil.getName());
+                    }
+                }
+            }
+            System.out.println("最小的文件为: "+minFile.getName()+"大小为："+min+"字节");
+            System.out.println("最大的文件为: "+maxFile.getName()+"大小为："+max+"字节");
+        }
+    }
+
+    private static void iteratorFile(File[] files) {
+        for(File f : files) {
+            if(f.isFile() && f.exists()) {
+                if(max < f.length()) {
+                    max = f.length();
+                    maxFile = f;
+                }
+                if(0 != f.length() && min > f.length()) {
+                    min = f.length();
+                    minFile = f;
+                }
+            } else if(f.isDirectory()) {
+                iteratorFile(f.listFiles());
+            }
+        }
+    }
+}
