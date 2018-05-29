9f0eb355979f7ba16ab1128865ba67a80de8f41c
fys <849485789@qq.com>
2018-05-30 00:16:59 +0800

    统计数字出现的概率

--- src/main/java/com/fys/collection/NumberProbility.java
+++ src/main/java/com/fys/collection/NumberProbility.java
@@ -10,22 +10,30 @@ import java.util.List;
  */
 public class NumberProbility {
     public static void main(String[] args) {
+        //前三个数
         int firstNum = 3;
         int secondNum = 1;
         int thirdNum = 4;
+        //统计的概率
         double probility;
+        //总共计算的次数
         double total = 1000000;
+        //3 1 4 出现的次数
         double count = 0;
         List<Integer> list = new ArrayList<>();
+        //初始化List
         for (int i = 0; i < 10; i++) {
             list.add(i);
         }
+        //执行total次shuffle并计算3 1 4 出现的次数
         for (int i = 0; i < total; i++) {
             Collections.shuffle(list);
+            //如果前三个数字为 3 1 4 则 增加一次次数
             if(firstNum == list.get(1) && secondNum == list.get(2) && thirdNum == list.get(3)) {
                 count++;
             }
         }
+        //计算概率
         probility = (count/total)*100;
         System.out.println("3 1 4 出现的次数为:"+ count +" 概率为:" + probility + "%");
     }
