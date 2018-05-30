360bdffa82642b1c984ecb04d3b513f60351f45d
fys <849485789@qq.com>
2018-05-30 17:55:05 +0800

    使用泛型简单实现栈

---
+++ src/main/java/com/fys/collection/MyStack.java
@@ -0,0 +1,32 @@
+package com.fys.collection;
+
+import java.util.LinkedList;
+
+/**
+ * 简单栈实现
+ */
+public class MyStack<T> {
+    LinkedList<T> linkedList = new LinkedList<T>();
+
+    public void push(T t) {
+        linkedList.addLast(t);
+    }
+
+    public void pull() {
+        linkedList.removeLast();
+    }
+
+    public T peek() {
+        return linkedList.getLast();
+    }
+
+    public static void main(String[] args) {
+        MyStack<String> stringStack = new MyStack();
+        stringStack.push("fys");
+        System.out.println(stringStack.peek());
+
+        MyStack<Integer> integerStack = new MyStack();
+        integerStack.push(9);
+        System.out.println(integerStack.peek());
+    }
+}
