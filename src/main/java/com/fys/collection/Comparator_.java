585f82b8a1de2669d73c95c0d015368da124a448
fys <849485789@qq.com>
2018-05-31 02:46:10 +0800

    比较器，使用lambda表达式

--- src/main/java/com/fys/collection/Comparator_.java
+++ src/main/java/com/fys/collection/Comparator_.java
@@ -24,12 +24,36 @@ public class Comparator_ {
 
         //如果不添加Comparator接口则会报错，因为People不是一个可比较的类型
         //另一种方法是让People实现Comparable接口，就不需要在此类中写内部类
+
+
         //lambda引用静态方法
-        Collections.sort(list, Comparator_::comparatorAge);
+//        Collections.sort(list, Comparator_::comparatorAgeByStatic);
+
+        //引用对象方法
+        Comparator_ comparator = new Comparator_();
+        Collections.sort(list, comparator::comparatorAgeByObject);
+
         System.out.println(list);
     }
 
-    private static int comparatorAge(People o1, People o2) {
+    /**
+     * lambda引用对象方法
+     * @param o1
+     * @param o2
+     * @return
+     */
+    private int comparatorAgeByObject(People o1, People o2) {
+        return o1.getAge() - o2.getAge();
+    }
+
+
+    /**
+     * lambda引用静态方法
+     * @param o1
+     * @param o2
+     * @return
+     */
+    private static int comparatorAgeByStatic(People o1, People o2) {
         return o1.getAge() - o2.getAge();
     }
 }
