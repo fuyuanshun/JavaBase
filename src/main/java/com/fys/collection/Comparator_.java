cd1a17d7de3e549f3a319febdc7cacc37525af29
fys <849485789@qq.com>
2018-05-30 17:39:35 +0800

    比较器

---
+++ src/main/java/com/fys/collection/Comparator_.java
@@ -0,0 +1,33 @@
+package com.fys.collection;
+
+import com.fys.bean.People;
+
+import java.util.*;
+
+/**
+ * 比较器
+ */
+public class Comparator_ {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int ran = 0;
+        List<People> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            ran = random.nextInt(30);
+            list.add(new People(i, "fys"+ran, (ran+1)*2));
+        }
+
+        //比较的策略
+        Comparator<People> comparator = (People o1, People o2) -> {
+            if (o1.getAge() > o2.getAge()) {
+                return 1;
+            }
+            return -1;
+        };
+
+        //如果不添加Comparator接口则会报错，因为People不是一个可比较的类型
+        //另一种方法是让People实现Comparable接口，就不需要在此类中写内部类
+        Collections.sort(list, comparator);
+        System.out.println(list);
+    }
+}
