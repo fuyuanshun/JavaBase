6ebe1ba3dbf8cbb818651b56ba9a88b5a2c041dc
fys <849485789@qq.com>
2018-06-04 01:31:11 +0800

    聊天机器人数据库表

--- src/main/java/com/fys/sql/robot.sql
+++ src/main/java/com/fys/sql/robot.sql
@@ -5,5 +5,5 @@ CREATE TABLE `dictionary` (
   `receive` varchar(100) DEFAULT NULL,
   `response` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 insert into dictionary (receive, response) values ('你好','好你妹'), ('操', '请说中国话'), ('大神', '请叫我大佬');
